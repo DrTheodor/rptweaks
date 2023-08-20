1106139899fc1813b1a5d05963272eb5d02ea8b2
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2023-08-20 10:01:26 +0300

    Fix mod menu integration

--- src/main/java/dev/drtheo/rptweaks/modmenu/RPTweaksMenu.java
+++ src/main/java/dev/drtheo/rptweaks/modmenu/RPTweaksMenu.java
@@ -12,20 +12,23 @@ public class RPTweaksMenu implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        Config config = Config.getConfig();
-        ConfigBuilder builder = ConfigBuilder.create().setSavingRunnable(config::save);
-        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+        return parent -> {
+            Config config = Config.getConfig();
+            ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent)
+                    .setSavingRunnable(config::save).setTitle(Text.of("Resource Pack Tweaks"));
 
-        ConfigCategory category = builder.getOrCreateCategory(Text.of("General"));
+            ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+            ConfigCategory category = builder.getOrCreateCategory(Text.of("General"));
 
-        category.addEntry(
-                entryBuilder.startBooleanToggle(
-                        Text.of("Preload Latest Resource Pack?"), config.shouldPreload()
-                ).setTooltip(
-                        Text.of("Makes Minecraft preload the latest resource pack before the game started!")
-                ).setSaveConsumer(config::shouldPreload).setDefaultValue(true).build()
-        );
+            category.addEntry(
+                    entryBuilder.startBooleanToggle(
+                            Text.of("Preload Latest Resource Pack?"), config.shouldPreload()
+                    ).setTooltip(
+                            Text.of("Makes Minecraft preload the latest resource pack before the game started!")
+                    ).setSaveConsumer(config::shouldPreload).setDefaultValue(true).build()
+            );
 
-        return parent -> ConfigBuilder.create().build();
+            return builder.build();
+        };
     }
 }
